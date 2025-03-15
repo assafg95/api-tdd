409dfe792eb1c08ab07f792e5ba4c623c6c3d74d
Assaf <assafgorovici@gmail.com>
2025-03-15 20:35:10 +0200

    tdd project init

---
+++ app.test.js
@@ -0,0 +1,19 @@
+const request = require('supertest');
+const app = require('./app');
+
+describe("Todos API", () => {
+    it('GET /todos --> array todos', () => {
+
+    });
+    it('GET /todos/id --> specific todo by Id', () => {
+
+    });
+    it('POST /todos --> add a new todo', () => {
+
+    });
+    it('GET /todos --> validates request body', () => {
+
+    });
+    
+});
+
