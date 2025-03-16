87cea360f6c5557f7ccb3d9a53dcb7055bcdaf76
Assaf <assafgorovici@gmail.com>
2025-03-16 11:15:48 +0200

    Get /todos test

--- app.test.js
+++ app.test.js
@@ -3,9 +3,25 @@ const app = require('./app');
 
 describe("Todos API", () => {
     it('GET /todos --> array todos', () => {
-
+        return request(app)
+            .get('/todos')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .then(response => {
+                expect(response.body).toEqual(
+                    expect.arrayContaining([
+                        expect.objectContaining({
+                            name: expect.any(String),
+                            completed: expect.any(Boolean)
+                        })
+                    ])
+                );
+            });
     });
     it('GET /todos/id --> specific todo by Id', () => {
+
+    });
+    it('Get /todos/id --> 404 if todo not found', () => {
 
     });
     it('POST /todos --> add a new todo', () => {
